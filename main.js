8d2763fbe749c00fe3c8f5da7f9ff5167d62ad4f
dawnxiaoming168 <dawnxiaoming168@gmail.com>
2025-02-12 16:28:16 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,361 @@
+let audioUrl = ""
+let audio = null
+let isPlaying = false
+//settings.json
+// Import the data to customize and insert them into page
+// 定义一个异步函数 fetchData 用于获取并处理 customize.json 文件中的数据
+const fetchData = () => {
+  // 使用 fetch API 获取 customize.json 文件
+  fetch("customize.json")
+    .then(data => data.json()) // 将获取的数据转换为 JSON 格式
+    .then(data => {
+      // 获取数据对象的键数组
+      dataArr = Object.keys(data)
+      // 遍历数据对象的键数组
+      dataArr.map(customData => {
+        // 检查当前键对应的值是否不为空字符串
+        if (data[customData] !== "") {
+          // 如果当前键是 "imagePath"
+          if (customData === "imagePath") {
+            // 获取具有 data-node-name 属性包含 "imagePath" 的元素，并设置其 src 属性
+            document
+              .querySelector(`[data-node-name*="${customData}"]`)
+              .setAttribute("src", data[customData])
+          // 如果当前键是 "fonts"
+          } else if (customData === "fonts") {
+            // 遍历字体数组
+            data[customData].forEach(font => {
+              // 创建一个新的 <link> 元素
+              const link = document.createElement('link')
+              link.rel = 'stylesheet' // 设置 <link> 元素的 rel 属性为 'stylesheet'
+              link.href = font.path // 设置 <link> 元素的 href 属性为字体的路径
+              document.head.appendChild(link) // 将 <link> 元素添加到 <head> 中
+              //设置body字体
+              document.body.style.fontFamily = font.name
+            })
+          } else if (customData === "music") {
+            audioUrl = data[customData]
+            audio = new Audio(audioUrl)
+            audio.preload = "auto"
+          } else {
+            document.querySelector(`[data-node-name*="${customData}"]`).innerText = data[customData]
+          }
+        }
+
+        // Check if the iteration is over
+        // Run amimation if so
+        if (dataArr.length === dataArr.indexOf(customData) + 1) {
+          document.querySelector("#startButton").addEventListener("click", () => {
+            document.querySelector(".startSign").style.display = "none"
+            animationTimeline()
+          }
+          )
+          // animationTimeline()
+        }
+      })
+    })
+}
+
+// Animation Timeline
+const animationTimeline = () => {
+  // Spit chars that needs to be animated individually
+  const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0]
+  const hbd = document.getElementsByClassName("wish-hbd")[0]
+
+  textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
+    .split("")
+    .join("</span><span>")}</span`
+
+  hbd.innerHTML = `<span>${hbd.innerHTML
+    .split("")
+    .join("</span><span>")}</span`
+
+  const ideaTextTrans = {
+    opacity: 0,
+    y: -20,
+    rotationX: 5,
+    skewX: "15deg"
+  }
+
+  const ideaTextTransLeave = {
+    opacity: 0,
+    y: 20,
+    rotationY: 5,
+    skewX: "-15deg"
+  }
+
+  const tl = new TimelineMax()
+
+  tl
+    .to(".container", 0.1, {
+      visibility: "visible"
+    })
+    .from(".one", 0.7, {
+      opacity: 0,
+      y: 10
+    })
+    .from(".two", 0.4, {
+      opacity: 0,
+      y: 10
+    })
+    .to(
+      ".one",
+      0.7,
+      {
+        opacity: 0,
+        y: 10
+      },
+      "+=2.5"
+    )
+    .to(
+      ".two",
+      0.7,
+      {
+        opacity: 0,
+        y: 10
+      },
+      "-=1"
+    )
+    .from(".three", 0.7, {
+      opacity: 0,
+      y: 10
+      // scale: 0.7
+    })
+    .to(
+      ".three",
+      0.7,
+      {
+        opacity: 0,
+        y: 10
+      },
+      "+=2"
+    )
+    .from(".four", 0.7, {
+      scale: 0.2,
+      opacity: 0
+    })
+    .from(".fake-btn", 0.3, {
+      scale: 0.2,
+      opacity: 0
+    })
+    .staggerTo(
+      ".hbd-chatbox span",
+      0.5,
+      {
+        visibility: "visible"
+      },
+      0.05
+    )
+    .to(".fake-btn", 0.1, {
+      backgroundColor: "#8FE3B6"
+    })
+    .to(
+      ".four",
+      0.5,
+      {
+        scale: 0.2,
+        opacity: 0,
+        y: -150
+      },
+      "+=0.7"
+    )
+    .from(".idea-1", 0.7, ideaTextTrans)
+    .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-2", 0.7, ideaTextTrans)
+    .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-3", 0.7, ideaTextTrans)
+    .to(".idea-3 strong", 0.5, {
+      scale: 1.2,
+      x: 10,
+      backgroundColor: "rgb(21, 161, 237)",
+      color: "#fff"
+    })
+    .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-4", 0.7, ideaTextTrans)
+    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(
+      ".idea-5",
+      0.7,
+      {
+        rotationX: 15,
+        rotationZ: -10,
+        skewY: "-5deg",
+        y: 50,
+        z: 10,
+        opacity: 0
+      },
+      "+=0.5"
+    )
+    .to(
+      ".idea-5 .smiley",
+      0.7,
+      {
+        rotation: 90,
+        x: 8
+      },
+      "+=0.4"
+    )
+    .to(
+      ".idea-5",
+      0.7,
+      {
+        scale: 0.2,
+        opacity: 0
+      },
+      "+=2"
+    )
+    .staggerFrom(
+      ".idea-6 span",
+      0.8,
+      {
+        scale: 3,
+        opacity: 0,
+        rotation: 15,
+        ease: Expo.easeOut
+      },
+      0.2
+    )
+    .staggerTo(
+      ".idea-6 span",
+      0.8,
+      {
+        scale: 3,
+        opacity: 0,
+        rotation: -15,
+        ease: Expo.easeOut
+      },
+      0.2,
+      "+=1"
+    )
+    .staggerFromTo(
+      ".baloons img",
+      2.5,
+      {
+        opacity: 0.9,
+        y: 1400
+      },
+      {
+        opacity: 1,
+        y: -1000
+      },
+      0.2
+    )
+    .from(
+      ".lydia-dp",
+      0.5,
+      {
+        scale: 3.5,
+        opacity: 0,
+        x: 25,
+        y: -25,
+        rotationZ: -45
+      },
+      "-=2"
+    )
+    .from(".hat", 0.5, {
+      x: -100,
+      y: 350,
+      rotation: -180,
+      opacity: 0
+    })
+    .staggerFrom(
+      ".wish-hbd span",
+      0.7,
+      {
+        opacity: 0,
+        y: -50,
+        // scale: 0.3,
+        rotation: 150,
+        skewX: "30deg",
+        ease: Elastic.easeOut.config(1, 0.5)
+      },
+      0.1
+    )
+    .staggerFromTo(
+      ".wish-hbd span",
+      0.7,
+      {
+        scale: 1.4,
+        rotationY: 150
+      },
+      {
+        scale: 1,
+        rotationY: 0,
+        color: "#ff69b4",
+        ease: Expo.easeOut
+      },
+      0.1,
+      "party"
+    )
+    .from(
+      ".wish h5",
+      0.5,
+      {
+        opacity: 0,
+        y: 10,
+        skewX: "-15deg"
+      },
+      "party"
+    )
+    .staggerTo(
+      ".eight svg",
+      1.5,
+      {
+        visibility: "visible",
+        opacity: 0,
+        scale: 80,
+        repeat: 3,
+        repeatDelay: 1.4
+      },
+      0.3
+    )
+    .to(".six", 0.5, {
+      opacity: 0,
+      y: 30,
+      zIndex: "-1"
+    })
+    .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
+    .to(
+      ".last-smile",
+      0.5,
+      {
+        rotation: 90
+      },
+      "+=1"
+    )
+
+  // tl.seek("currentStep");
+  // tl.timeScale(2);
+
+  // Restart Animation on click
+  const replyBtn = document.getElementById("replay")
+  replyBtn.addEventListener("click", () => {
+    tl.restart()
+
+  })
+}
+
+// Run fetch and animation in sequence
+fetchData()
+
+const playPauseButton = document.getElementById('playPauseButton')
+
+document.getElementById('startButton').addEventListener('click', () => {
+  if (audio) {
+    togglePlay(true)
+  }
+})
+
+playPauseButton.addEventListener('click', () => {
+  if (audio) {
+    togglePlay(!isPlaying)
+  }
+})
+
+function togglePlay(play) {
+  if (!audio) return
+  
+  isPlaying = play
+  play ? audio.play() : audio.pause()
+  playPauseButton.classList.toggle('playing', play)
+}
